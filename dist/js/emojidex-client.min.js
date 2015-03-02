4930331a64c255c34f39444340a393b2e9f2415c
snowsunny <snowsunny1107@gmail.com>
2015-03-02 16:21:25 +0900

    update version

--- dist/js/emojidex-client.min.js
+++ dist/js/emojidex-client.min.js
@@ -1,5 +1,5 @@
 /*
- * emojidex client - v0.3.3
+ * emojidex client - v0.3.4
  * * Provides search, index caching and combining and asset URI resolution
  * https://github.com/emojidex/emojidex-web-client
  *
