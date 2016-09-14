ad643f9c5bc90d47fb49cd9f3973abfcfe7a9966
影月 零 <zero@phantom.industries>
2016-09-14 21:00:39 +0900

    remove export statements

--- spec/helpers/data.js
+++ spec/helpers/data.js
@@ -1,30 +1,30 @@
-export let emoji_kiss = {
+let emoji_kiss = {
   code: 'kiss',
   moji: '💋',
   unicode: '1f48b',
   category: 'objects'
 };
 
-export let emoji_kissing = {
+let emoji_kissing = {
   code: 'kissing',
   moji: '😗',
   unicode: '1f617',
   category: 'faces'
 };
 
-export let emoji_emoji = {
+let emoji_emoji = {
   code: 'emoji',
   category: 'symbols'
 };
 
-export let emoji_dragon = {
+let emoji_dragon = {
   code: 'dragon',
   moji: '🐉',
   unicode: '1f409',
   category: 'nature'
 };
 
-export let test_user_info = {
+let test_user_info = {
   auth_user: 'test',
   auth_token: '1798909355d57c9a93e3b82d275594e7c7c000db05021138'
 };
