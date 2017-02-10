8a87c4c9106c93f73b8521ceb37c9b95b4245474
zero <zero@phantom.industries>
2017-02-10 17:40:55 +0900

    add explanations to style check overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,14 @@ module.exports = {
         "import"
     ],
     "rules": {
+      // Class names are Pascal
+      // Function and Object names are Camel
+      // Attribute and Cariable names are Snake
       "camelcase": [0, { "properties": "never" }],
+      // Sometimes arrays and hashes could be copied to JSON files or a different language.
+      // Disallowing dangling commas will prevent errors in these cases.
       "comma-dangle": ["error", "never"],
+      // We denote privates and reserved values with a leading _.
       "no-underscore-dangle": [0]
     }
 };
