de39e340e7775d76b5daa46f47c7124d9008f015
Rika Yoshida <nazuki@phantom.industries>
2018-07-13 16:30:57 +0900

    update version 0.18.2

--- dist/js/emojidex-client.js
+++ dist/js/emojidex-client.js
@@ -1,5 +1,5 @@
 /**
-,* emojidex client - v0.18.1
+,* emojidex client - v0.18.2
 ,* * Provides search, index caching and combining and asset URI resolution
 ,* https://github.com/emojidex/emojidex-web-client
 ,*
