d3742cb1f342872729d16c027a1a915aed087d36
Rika Yoshida <nazuki@phantom.industries>
2019-07-29 16:58:13 +0900

    use async/await (data.js/_storage.js)

--- spec/data.spec.js
+++ spec/data.spec.js
@@ -36,28 +36,26 @@ describe('EmojidexData', () => {
         expect(ECSpec.Data.storage.get('emojidex.auth_info')).toEqual(ECSpec.Data.defaultAuthInfo)
         done()
       })
-      it('emojidex.moji_codes', done => {
-        axios.get(`${ECSpec.apiUrl}moji_codes`).then(response => {
-          expect(ECSpec.Data.storage.get('emojidex.moji_codes')).toEqual(response.data)
-          done()
-        })
+      it('emojidex.moji_codes', async done => {
+        const response = await axios.get(`${ECSpec.apiUrl}moji_codes`)
+        expect(ECSpec.Data.storage.get('emojidex.moji_codes')).toEqual(response.data)
+        done()
       })
     })
 
-    it('after', done => {
+    it('after', async done => {
       expect(ECSpec.Data.storage.isEmpty('emojidex')).toBe(false)
-      expect(ECSpec.Data.storage.keys('emojidex')).toEqual(['moji_codes', 'emoji', 'history', 'favorites', 'categories', 'auth_info', 'moji_codes_updated'])
+      const keys = await ECSpec.Data.storage.keys('emojidex')
+      expect(keys).toEqual(['moji_codes', 'emoji', 'history', 'favorites', 'categories', 'auth_info', 'moji_codes_updated'])
       expect(ECSpec.Data.storage.isSet('emojidex')).toBe(true)
       done()
     })
 
-    it('clear', done => {
-      ECSpec.Data.storage.clear().then(() => {
-        return ECSpec.Data.storage.hub.get('emojidex')
-      }).then(data => {
-        expect(data).toEqual(null)
-        done()
-      })
+    it('clear', async done => {
+      await ECSpec.Data.storage.clear()
+      const data = await ECSpec.Data.storage.hub.get('emojidex')
+      expect(data).toEqual(null)
+      done()
     })
   })
 })
