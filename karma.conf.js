b4b72fe3f1e8a65ae9f7d2a9ce57ef8ee988ce29
Rika Yoshida <nazuki@phantom.industries>
2019-09-04 10:26:26 +0900

    change timeout config

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config) {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+    browserNoActivityTimeout: 60000,
 
 
     // frameworks to use
